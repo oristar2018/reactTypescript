04c98e66256d2e5d8bc291c71b00a55ce045665b
Oristar2018 <ulricvanstatter@gmail.com>
2019-02-12 21:54:24 +0100

    state and props need to be defined through interfaces for example in order to avoid the caveat of readonly{} definition, this participates in a builder pattern model for state and props

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+//declaring an interface for state is necessary due to default readonly state { }
+interface State { name: string } 
+interface Props { }
+class App extends Component<Props, State> {
+
+  constructor(props:any) {
+      super(props);
+      this.state = { 
+        name: "test"
+      }
+      
+    }
+
+
+
+    componentDidMount() {
+
+     
+
+    }
+
   render() {
+
+     let Name : string = "testString";
+      
+      
+
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.name}
           </p>
+          <button onClick={() => {
+
+            const testFunction = (name: string) => {
+            this.setState({ name })
+          return Name = name 
+      };
+
+      console.log(testFunction("benoit"));
+
+          }}></button>
           <a
             className="App-link"
             href="https://reactjs.org"
